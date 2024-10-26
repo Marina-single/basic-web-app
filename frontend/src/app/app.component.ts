f04ca18348749b81e02938f606356cfb24769381
Marisha <marinasemenovaa1@gmail.com>
2024-10-26 18:37:38 +0200

    add contact-us component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [
+    RouterOutlet,
+    HeaderComponent,
+    ContactUsComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })

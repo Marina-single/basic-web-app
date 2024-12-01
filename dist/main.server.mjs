324197bea48df33d5b77b3f3c870d2a654778ce3
Maria Okatova <mary.okatova@gmail.com>
2024-12-01 16:29:16 +0100

    fix href

--- dist/main.server.mjs
+++ dist/main.server.mjs
@@ -1,2 +1,2 @@
 import './polyfills.server.mjs';
-import{a}from"./chunk-5J3NMSCN.mjs";import"./chunk-CAANZGZ2.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
+import{a}from"./chunk-ICCWIRGR.mjs";import"./chunk-CAANZGZ2.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
